65e6b871275e637a3df07906a97332a415567262
flye <{ID}+{username}@users.noreply.github.com>
2026-07-11 14:35:23 +0200

    fix: set ZIG_GLOBAL_CACHE_DIR in nix flake build

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
           nativeBuildInputs = [ pkgs.zig_0_16 ];
           dontConfigure = true;
           buildPhase = ''
+            export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
             zig build -Doptimize=ReleaseSmall
           '';
           installPhase = ''
