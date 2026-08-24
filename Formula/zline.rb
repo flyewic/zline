ae1973466b40a41193b327de7bf11b261336d765
flyewic <22092995+flyewic@users.noreply.github.com>
2026-08-24 19:16:49 +0000

    chore: update packaging for v0.5.0 [skip ci]

--- Formula/zline.rb
+++ Formula/zline.rb
@@ -1,25 +1,25 @@
 class Zline < Formula
   desc "fast line counter in zig"
   homepage "https://github.com/flyewic/zline"
-  url "https://github.com/flyewic/zline/releases/download/v0.4.3/zline-x86_64-linux"
-  sha256 "de443e50589340c8cff1464aafe33588460066effa63a979754c82c9f1b6ad9c"
-  version "0.4.3"
+  url "https://github.com/flyewic/zline/releases/download/v0.5.0/zline-x86_64-linux"
+  sha256 "1316ca8413299fca09ea2d2cdc357c37e4cb441670067252f9232d392d6c13b0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/flyewic/zline/releases/download/v0.4.3/zline-aarch64-macos"
-      sha256 "5a6d3d1bed0eb02930d30928afa880c756a9b59cdd9943ee34eed684fe431433"
+      url "https://github.com/flyewic/zline/releases/download/v0.5.0/zline-aarch64-macos"
+      sha256 "e54549531d8139e2d18f9c7ea6f5640fa52f84d51e3f41e2dca662be6c6ab765"
     else
-      url "https://github.com/flyewic/zline/releases/download/v0.4.3/zline-x86_64-macos"
-      sha256 "3768b033f1d4d4d5dc25d9d34731d04e9ef98eff7bd5ed1385b273a9ecf4d87d"
+      url "https://github.com/flyewic/zline/releases/download/v0.5.0/zline-x86_64-macos"
+      sha256 "a1d1a21c2de578ce3e5274381285ec16adfceb48f0a56323f75e7d2fb90f6fc9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/flyewic/zline/releases/download/v0.4.3/zline-aarch64-linux"
-      sha256 "e3dd0d107aab85a3d1bfd0ca43e959b86826d21bc244e72fd25d85ee783a314e"
+      url "https://github.com/flyewic/zline/releases/download/v0.5.0/zline-aarch64-linux"
+      sha256 "69ef6956f7384b1f8eb61551cb9ac7d7656f7424a4de7527de38212918b50921"
     end
   end
 
