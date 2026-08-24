8b0865dcdc6b307ac06f8f578dddfcd7def770d5
flye <flye@users.noreply.github.com>
2026-08-24 21:14:55 +0200

    chore: bump version to 0.5.0

--- src/cli.zig
+++ src/cli.zig
@@ -191,7 +191,7 @@ pub fn printHelp(io: Io) void {
     w.flush() catch {};
 }
 
-pub const version = "0.4.3";
+pub const version = "0.5.0";
 
 pub fn printVersion(io: Io) void {
     var buf: [64]u8 = undefined;
